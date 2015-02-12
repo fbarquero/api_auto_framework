71d1824ef33a6ae0ef0da0d9e23c4b9f9cfa57f3
fbarquero <fbarquero86@gmail.com>
2015-02-11 18:39:48 -0600

    trying to make parallel test run even with failed tcs

--- tests/module_for_test.py
+++ tests/module_for_test.py
@@ -1,19 +1,20 @@
-import _multiprocessing
-
 __author__ = 'it'
+
 from utils import base_test
-import unittest
 import time
-from nose.plugins.multiprocess import MultiProcess
+
+import unittest
+
+
 #@unittest.skip("testing the skip functionality")
 class testing(base_test.base_test):
-    #to allow the test of this module to be distributed alogn the running processes of Nose
-    _multiprocess_shared_ = True
     def test_1(self):
         stdin, stdout, stderr = self.ssh_session.exec_command('ps -A x |grep sshd |grep -v grep')
         print stdout.readlines()
         time.sleep(5)
         print "sleeped 10s"
+        self.assert_(5 == 4, msg="text")
+
 
     def test_2(self):
         stdin, stdout, stderr = self.ssh_session.exec_command('ps -A x |grep sshd |grep -v grep')
@@ -38,19 +39,6 @@ class testing(base_test.base_test):
         print stdout.readlines()
         time.sleep(5)
         print "sleeped 10s"
-
-def suite():
-    """
-    Construct a test suite
-    """
-    test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(testing))
-    return test_suite
-
-runner = unittest.TextTestRunner()
-runner.run(suite())
-
-
 
 
 
--- utils/base_test.py
+++ utils/base_test.py
@@ -2,20 +2,18 @@ from paramiko.ssh_gss import _SSH_GSSAPI
 
 __author__ = 'Mordigan'
 
-#url = http://segfault.in/2010/03/paramiko-ssh-and-sftp-with-python/
+# url = http://segfault.in/2010/03/paramiko-ssh-and-sftp-with-python/
 import unittest
 from connection import ssh_connect
 
 
 class base_test(unittest.TestCase):
+    _multiprocess_can_split_ = True
 
     def setUp(self):
         self._ssh_connection = ssh_connect.SshConnection()
-        self.ssh_session = self._ssh_connection.open_connection('root', 'meds22', '10.111.2.13', 30)
+        self.ssh_session = self._ssh_connection.open_connection('root', 'meds22', '10.111.2.214', 30)
 
 
     def tearDown(self):
         self._ssh_connection.close_ssh_connection(self.ssh_session)
-
-
-
