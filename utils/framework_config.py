bb6d0bffb655d55aeb7483510fc4eb5aacf3c931
fbarquero <fbarquero86@gmail.com>
2015-02-16 01:04:12 -0600

    config file

---
+++ utils/framework_config.py
@@ -0,0 +1,10 @@
+__author__ = 'Mordigan'
+
+class GlobalConstants:
+    HOSTNAME = '10.111.2.214'
+    USERNAME = 'root'
+    PASSWORD = 'meds22'
+    CC_URL = 'https://%s' % HOSTNAME
+
+class ServicedConstants:
+    pass
